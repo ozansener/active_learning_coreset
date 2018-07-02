7b45cba12cf370697e1f582964c1bcc4eb25b9ad
Ozan Sener <ozansener@gmail.com>
2018-07-02 11:34:21 +0200

    Update greedy_facility_location.py

--- coreset/greedy_facility_location.py
+++ coreset/greedy_facility_location.py
@@ -5,10 +5,11 @@ import time
 import pickle
 import bisect
 
-dat = pickle.load(open('chosen_100_data_49871_True'))
-data = numpy.concatenate((dat['a'][0],dat['a'][3]), axis=0)
-budget = 20000
+dat = pickle.load(open('representations_saved'))
+#data = numpy.concatenate((dat['a'][0],dat['a'][3]), axis=0)
+budget = 20000 # k in the paper
 
+# Compute Pairwise Distances between all points in 3 BLAS call
 start = time.clock() 
 num_images = data.shape[0]
 dist_mat = numpy.matmul(data,data.transpose())
@@ -17,9 +18,8 @@ sq = numpy.array(dist_mat.diagonal()).reshape(num_images,1)
 dist_mat *= -2
 dist_mat+=sq
 dist_mat+=sq.transpose()
-
 elapsed = time.clock() - start
-print "Time spent in (function name) is: ", elapsed
+print "Time spent in (computing all pairwise l2 distances) is: ", elapsed
 
 start = time.clock()
 num_data = dist_mat.shape[0]
@@ -27,10 +27,12 @@ candidate_ids = range(num_data)
 subset = []
 
 # first insert all labeled ones
-for lab in range(1): #dat['gt_f'].shape[0]):
+# This is the initial set S in the paper
+for lab in dat['gt_f'].shape[0]):
     i_star = lab
     subset.append(i_star)
 
+# Greedy facility location
 k = 0
 while k < budget:
     no = numpy.argmax(d)
